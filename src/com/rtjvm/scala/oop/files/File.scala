74b7ba1f4768ad0001ad8cedb9e30f9357ea1a96
PatMahoney <ziolkowski@gmail.com>
2019-01-01 19:49:52 +0100

    Added first version of Filesystem with .gitignore file.

---
+++ src/com/rtjvm/scala/oop/files/File.scala
@@ -0,0 +1,21 @@
+package com.rtjvm.scala.oop.files
+
+import com.rtjvm.scala.oop.filesystem.FilesystemException
+
+class File(override val parentPath: String, override val name: String, contents: String) extends DirEntry(parentPath, name) {
+
+  override def asDirectory: Directory =
+    throw new FilesystemException("File cannot be converted to a directory!")
+  override def asFile: File = this
+  override def getType: String = "File"
+
+  override def isDirectory: Boolean = false
+  override def isFile: Boolean = true
+
+}
+
+object File {
+
+  def empty(parentPath: String, name: String): File =
+    new File(parentPath, name, "")
+}
